82485db7f56e966a44bb8fc975828f691b081fdf
Tim McEwan <tim@mcewan.it>
2011-06-28 00:08:43 +1000

    Initial import.

---
+++ config/initializers/hoptoad.rb
@@ -0,0 +1,3 @@
+HoptoadNotifier.configure do |config|
+  config.api_key = 'e4c4b8a954ab0303ceb525e052f3e86d'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coffeeprefs::Application.config.secret_token = 'd2832eed6a64621c7f3f25e27cfc704f1af6039c2dc5b9124c9d4a9ab2d8eb84261aefa88c66e1b4fd506e43f72e807326bcde9b67c7230fbfa3da5507f6d1cf'
