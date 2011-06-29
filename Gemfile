394586e089b6704a8edde6496f0da5dbae9a91f3
Chelsea Robb <chelsea.robb@gmail.com>
2011-06-29 19:25:58 +1000

    Models

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem "hoptoad_notifier"
 gem "jammit"
 gem "jquery-rails"
 gem "bson_ext"
-
+gem 'inherited_resources'
 group :development, :test do
   gem 'ruby-debug19', :require => 'ruby-debug'
   gem "rspec-rails", ">= 2.0.1"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User
   include Mongoid::Document
+  field :name, :type => String
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  embeds_many :beverage_preferences
+  emedded_in :organisation
 
 end
