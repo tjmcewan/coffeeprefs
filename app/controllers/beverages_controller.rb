9d1c8f49b4f49d4987f25e2eecd2e4c3f08886c1
Chelsea Robb <chelsea.robb@gmail.com>
2011-06-29 19:57:11 +1000

    Moved custom create to beverage_prefs

--- app/controllers/beverages_controller.rb
+++ app/controllers/beverages_controller.rb
@@ -1,15 +1,3 @@
 class BeveragesController < InheritedResources::Base
-  actions :all, :except => [:create, :update]
-  
-  def create
-    unless @beverage = Beverage.where(:name => params[:beverage_name])
-      @beverage = Beverage.create(:name => params[:beverage_name])
-    end
 
-    render :json => BeveragePreference.create(:beverage => @beverage, 
-                      :user => current_user, :preferences => params[:preferences])
-  end
-  
-  def update
-  end
 end
