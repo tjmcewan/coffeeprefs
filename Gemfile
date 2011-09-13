6e25e0fd7e7e9bc215720a10c8b2525c469ac998
Tim McEwan <tim@mcewan.it>
2011-09-13 22:24:51 +1000

    asset manifests & template parsing

--- Gemfile
+++ Gemfile
@@ -21,4 +21,5 @@ group :assets do
   gem 'sass-rails', "  ~> 3.1.0"
   gem 'coffee-rails', "~> 3.1.0"
   gem 'uglifier'
+  gem 'ejs'
 end
---
+++ vendor/assets/javascripts/vendor.js
@@ -0,0 +1,8 @@
+//= require yepnope
+//= require modernizr
+//= require jquery
+//= require jquery_ujs
+//= require underscore
+//= require backbone
+//= require backbone.localStorage
+//= require_tree .
