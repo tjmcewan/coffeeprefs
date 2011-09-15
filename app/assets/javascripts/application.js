5ab51b0b14df1078fe97625b3479a986c053c677
Tim McEwan <tim@mcewan.it>
2011-09-15 16:04:44 +1000

    misunderstood how pipeline & manifests work

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,9 @@
+//= require yepnope
+//= require modernizr
+//= require jquery
+//= require jquery_ujs
+//= require underscore
+//= require backbone
+//= require backbone-localStorage
 //= require_tree ../templates
 //= require_tree ./backbone
-//= require_tree .
--- vendor/assets/javascripts/vendor.js
+++
@@ -1,8 +0,0 @@
-//= require yepnope
-//= require modernizr
-//= require jquery
-//= require jquery_ujs
-//= require underscore
-//= require backbone
-//= require backbone.localStorage
-//= require_tree .
